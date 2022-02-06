fefd8b3d694317b5c0eac5c375f3669e8fce124f
Dhans <sgdhans@gmail.com>
2022-02-06 15:03:14 +0530

    working queries with graphql

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
+import { __prod__ } from "./constants";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
-  const post = orm.em.create(Post, { title: "my first post" });
-  await orm.em.persistAndFlush(post);
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(2000, () => {
+    console.log("listening on port 2000");
+  });
 };
 
 main().catch((err) => {
