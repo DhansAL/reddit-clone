f03152388a94a903088c53d0298d9c0495c6f0eb
Dhans <sgdhans@gmail.com>
2022-02-08 14:19:19 +0530

    form component

--- redditclient/src/pages/register.tsx
+++ redditclient/src/pages/register.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Form, Formik } from 'formik'
     ;
+import { Box, Button } from '@chakra-ui/react';
+import { Wrapper } from '../Components/Wrapper';
+import { InputField } from '../Components/InputField';
 interface registerProps {
 
 }
@@ -9,13 +12,26 @@ const Register: React.FC<registerProps> = ({
 
 }) => {
     return (
-        <Formik initialValues={{ username: "", password: "" }}>
-            {() => (
-                <Form>
-                    <div className="">hello </div>
-                </Form>
-            )}
-        </Formik>
+        <Wrapper variant='small'>
+            <Formik initialValues={{ username: "", password: "" }} onSubmit={(values) => {
+                console.log(values);
+
+            }}>
+                {({ isSubmitting }) => (
+                    <Form>
+                        <InputField name='username' placeholder='username' label='Username' />
+                        <Box mt={4}>
+                            <InputField name='password' placeholder='password' label='Password' type="password" />
+
+                        </Box>
+                        <Button type="submit" mt={4}
+                            isLoading={isSubmitting}
+                            colorScheme='teal'>register</Button>
+                    </Form>
+                )}
+            </Formik>
+        </Wrapper>
+
     )
 }
 export default Register;
