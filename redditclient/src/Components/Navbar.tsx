d38a9233b2e490e0b64558006b16d4124121378a
Dhans <sgdhans@gmail.com>
2022-02-09 12:29:47 +0530

    logout component

--- redditclient/src/Components/Navbar.tsx
+++ redditclient/src/Components/Navbar.tsx
@@ -1,46 +1,48 @@
-import { Box, Button, Flex } from '@chakra-ui/react';
-import { Link } from '@chakra-ui/react'
-import React from 'react';
-import NextLink from 'next/link';
-import { useMeQuery } from '../generated/graphql';
+import { Box, Button, Flex } from "@chakra-ui/react";
+import { Link } from "@chakra-ui/react";
+import React from "react";
+import NextLink from "next/link";
+import { useLogoutMutation, useMeQuery } from "../generated/graphql";
 
 export const Navbar: React.FC = () => {
-    //whats the first arg?
-    const [{ data, fetching }] = useMeQuery()
+  //whats the first arg?
+  const [{ data, fetching }] = useMeQuery();
+  const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
 
-    let body = null;
-    if (fetching) {
+  let body = null;
+  if (fetching) {
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={2}>login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link mr={2}>register</Link>
+        </NextLink>
+      </>
+    );
+  } else {
+    body = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button
+          onClick={() => {
+            logout();
+            console.log("logout");
+          }}
+          isLoading={logoutFetching}
+          variant="link "
+        >
+          logout
+        </Button>
+      </Flex>
+    );
+  }
 
-    } else if (!data?.me) {
-        body = (
-            <>
-                <NextLink href='/login'>
-                    <Link mr={2}>
-                        login
-                    </Link>
-                </NextLink>
-                <NextLink href='/register'>
-                    <Link mr={2}>
-                        register
-                    </Link>
-                </NextLink>
-            </>
-        )
-    } else {
-        body = (
-            <Flex>
-                <Box mr={2}>{data.me.username}</Box>
-                <Button variant="link ">logout</Button>
-
-            </Flex>
-        )
-    }
-
-    return (
-        <Flex bg="tomato" p={4}>
-            <Box ml={'auto'}>
-                {body}
-            </Box>
-        </Flex>
-    )
+  return (
+    <Flex bg="tomato" p={4}>
+      <Box ml={"auto"}>{body}</Box>
+    </Flex>
+  );
 };
